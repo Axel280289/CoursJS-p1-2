549dbc89e32003f2317dafb677e12ce311c00bb9
Axel280289 <beauvois.axel2802@gmail.com>
2023-08-29 14:26:11 +0200

    AjoutExo Generateur de bulles

---
+++ Bubble/index.js
@@ -0,0 +1,35 @@
+const counterDisplay = document.querySelector("h3");
+let counter = 0;
+
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
+
+  // Math random pour definir une taille aleatoire sur cet exemple la taille
+  // sera comprise entre 100 et 300px de large et de hauteur
+
+  const size = Math.random() * 100 + 68 + "px";
+  bubble.style.height = size;
+  bubble.style.width = size;
+
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
+  // Attention ! Math toujours en majuscule car c'est un Objet
+
+  //   ternaire plusMinus
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+
+  bubble.addEventListener("click", () => {
+    counter++;
+    counterDisplay.textContent = counter;
+    bubble.remove();
+  });
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+
+setInterval(bubbleMaker, 250);
